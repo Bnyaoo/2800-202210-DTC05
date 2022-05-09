4cd7302ca8d8a38999d4bc104e2490244b6816ae
TheRedLegionRises <jerry000@hotmail.ca>
2022-05-09 16:27:40 -0700

    added mongoDB connection

--- public/code.js
+++ public/code.js
@@ -2,8 +2,8 @@ function login(){
     email = $("#email").val();
     password = $("#password").val();
 
-    console.log(email);
-    console.log(password);
+    // console.log(email);
+    // console.log(password);
 
 
     window.location.href=`http://localhost:5000/login/${email}/${password}`;
--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const express = require('express')
 const app = express()
+const https = require('https');
+const bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://COMP2800PROJECT:COMP2800@cluster0.rnbqx.mongodb.net/Project2800?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema({
+    user: String
+});
+
+const unicornModel = mongoose.model("users", userSchema);
 
 var session = require('express-session')
 
@@ -8,10 +24,10 @@ app.set('view engine', 'ejs')
 // Use the session middleware
 app.use(session({ secret: 'ssshhhhh', saveUninitialized: true, resave: true }));
 
-users = {
-    "user1": "pass1",
-    "user2": "pass2",
-}
+// users = {
+//     "user1": "pass1",
+//     "user2": "pass2",
+// }
 
 
 app.listen(5000, function (err) {
@@ -32,20 +48,37 @@ app.get('/login/', function (req, res, next) {
 })
 
 app.get('/login/:user/:pass', function (req, res, next) {
-    if (users[req.params.user] == req.params.pass) {
-        req.session.authenticated = true
-        req.session.user = req.params.user
-        // res.send(`Successful Login! Welcome, ${req.session.user}`);
-        // res.render("profile.ejs", {
-        res.redirect('/login_success.html')
+    console.log(req.params.user);
+    username = req.params.user;
+    console.log(req.params.pass);
+    password = req.params.pass;
+    // res.send("HI");
 
-        //     "id": req.params.user,
-        // });
+    unicornModel.find({ user: `${username}` }, function (err, results) {
+        if (err) {
+            console.log("Error " + err);
+        } else {
+            console.log("Data " + results);
+        }
+        res.send(results);
+    });
 
-    } else {
-        req.session.authenticated = false
-        res.send("Failed Login!")
-    }
+
+    // if (users[req.params.user] == req.params.pass) {
+    //     req.session.authenticated = true
+    //     req.session.user = req.params.user
+    //     // res.send(`Successful Login! Welcome, ${req.session.user}`);
+    //     // res.render("profile.ejs", {
+
+    //     res.redirect('/login_success.html')
+
+    //     //     "id": req.params.user,
+    //     // });
+
+    // } else {
+    //     req.session.authenticated = false
+    //     res.send("Failed Login!")
+    // }
 
 })
 
