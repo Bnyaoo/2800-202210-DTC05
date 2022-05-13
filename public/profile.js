c7123dbe17b92f194e4294c68e29efe7c0339687
TheRedLegionRises <jerry000@hotmail.ca>
2022-05-13 01:57:58 -0700

    heroku upload #2

--- public/profile.js
+++ public/profile.js
@@ -6,19 +6,19 @@ function send_profile(){
     console.log(lowerLimit);
     console.log(upperLimit);
 
-    $.ajax(
-        {
-            url: "https://glacial-wave-58910.herokuapp.com/findUnicornByWeight",
-            type: "POST",
-            data: {
-                "lowerLimit": lowerLimit,
-                "upperLimit": upperLimit
-            },
-            success: process_res
-        }
-    )
-}
-    //window.location.href="http://localhost:5000/profileUpdateSuccess";
+//     $.ajax(
+//         {
+//             url: "https://glacial-wave-58910.herokuapp.com/findUnicornByWeight",
+//             type: "POST",
+//             data: {
+//                 "lowerLimit": lowerLimit,
+//                 "upperLimit": upperLimit
+//             },
+//             success: process_res
+//         }
+//     )
+// }
+    window.location.href="https://pure-depths-86049.herokuapp.com/profileUpdateSuccess";
 
 
 
